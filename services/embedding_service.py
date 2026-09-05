d97a018c597ad808a16d3aa7cb9d86a81b8a0faf
piyushb956 <piyushrocking116@gmail.com>
2026-09-05 16:06:34 +0530

    change embedding using fastembed

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -1,9 +1,12 @@
-from sentence_transformers import SentenceTransformer
+from fastembed import TextEmbedding
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+_model = None
+
+def get_model():
+    global _model
+    if _model is None:
+        _model = TextEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return _model
 
 def generate_embedding(text):
-
-    return model.encode(text)
+    return list(get_model().embed([text]))[0]
